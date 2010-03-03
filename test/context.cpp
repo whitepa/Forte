2c8c9ef177ac4be457d11dcb83d50f2943e15057
whitepa <whitepa@038091cf-3c17-4aaf-a619-8414e6e1260f>
2010-03-03 17:43:13 +0000

    adding unit test for Context class
    git-svn-id: svn+ssh://svn/repos/scale-product/trunk@8193 038091cf-3c17-4aaf-a619-8414e6e1260f

---
+++ test/context.cpp
@@ -0,0 +1,60 @@
+#include "Forte.h"
+#define BOOST_TEST_MODULE "Context Unit Tests"
+#include <boost/test/unit_test.hpp>
+
+
+struct ContextFixture {
+    Forte::Context c;
+};
+
+class TestClass {
+public:
+    TestClass() { ++mCount; }
+    virtual ~TestClass() { --mCount; }
+    int getCount(void) { return mCount; }
+    static int mCount;
+};
+int TestClass::mCount = 0;
+
+BOOST_FIXTURE_TEST_SUITE( context, ContextFixture );
+BOOST_AUTO_TEST_CASE ( context_test1 )
+{
+    CLogManager logMgr;
+    logMgr.BeginLogging();
+    logMgr.SetGlobalLogMask(HLOG_ALL);
+
+    // verify invalid keys throws
+    BOOST_CHECK_THROW(c.GetRef<CLogManager>("forte.logManager"), EInvalidKey);
+    
+    c.Set("forte.config", new CServiceConfig());
+
+    BOOST_CHECK_THROW(c.GetRef<CServiceConfig>("forte.config.fail"), EInvalidKey);
+
+    // verify type mismatch throws
+    BOOST_CHECK_THROW(c.GetRef<CLogManager>("forte.config"), ETypeMismatch);
+
+    {
+        CServiceConfig &cfg(c.GetRef<CServiceConfig>("forte.config"));
+        cfg.Set("key", "value");
+    }
+
+    // verify objects are destroyed and reference counts work correctly
+    BOOST_CHECK(TestClass::mCount == 0);
+    c.Set("testobject", new TestClass());
+    BOOST_CHECK(TestClass::mCount == 1);
+    c.Remove("testobject");
+    BOOST_CHECK(TestClass::mCount == 0);
+    BOOST_CHECK_THROW(c.GetRef<TestClass>("testobject"), EInvalidKey);
+
+    c.Set("testobject", new TestClass());
+    BOOST_CHECK(TestClass::mCount == 1);
+    {
+        Forte::Context::ObjectPtr o(c.Get<TestClass>("testobject"));
+        c.Remove("testobject");
+        BOOST_CHECK( CAST(TestClass, o).getCount() == 1 );
+        BOOST_CHECK( TestClass::mCount == 1 );
+    }
+    BOOST_CHECK(TestClass::mCount == 0);
+}
+BOOST_AUTO_TEST_SUITE_END();
+
