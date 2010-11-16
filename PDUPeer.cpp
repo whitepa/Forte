0706144f3efc02580462b111abf651babac37c08
Nate Hardt <nate@scalecomputing.com>
2010-11-16 02:23:17 -0800

    pulled in Forte PDUPeer classes and related changes

---
+++ PDUPeer.cpp
@@ -0,0 +1,80 @@
+#include "AutoMutex.h"
+#include "LogManager.h"
+#include "PDUPeer.h"
+#include "Types.h"
+
+void Forte::PDUPeer::DataIn(size_t len, char *buf)
+{
+    AutoUnlockMutex lock(mLock);
+    if ((mCursor + len) > mBufSize)
+        throw EPeerBufferOverflow();
+    memcpy(mPDUBuffer.get() + mCursor, buf, len);
+    hlog(HLOG_DEBUG2, "received data; oldCursor=%llu newCursor=%llu",
+         (u64) mCursor, (u64) mCursor + len);
+    mCursor += len;    
+}
+
+void Forte::PDUPeer::SendPDU(const Forte::PDU &pdu) const
+{
+    AutoUnlockMutex lock(mLock);
+    size_t len = sizeof(Forte::PDU) - Forte::PDU::PDU_MAX_PAYLOAD;
+    len += pdu.payloadSize;
+    if (len > sizeof(Forte::PDU))
+        throw EPeerSendInvalid();
+    if (send(mFD, &pdu, len, 0) < 0)
+        throw EPeerSendFailed(FStringFC(), "%s", strerror(errno));
+}
+
+bool Forte::PDUPeer::IsPDUReady(void) const
+{
+    AutoUnlockMutex lock(mLock);
+    return lockedIsPDUReady();
+}
+bool Forte::PDUPeer::lockedIsPDUReady(void) const
+{
+    // returns true if a PDU has been received
+    //         false otherwise
+    // ('out' will be populated with the PDU if true)
+        
+    // check for a valid PDU
+    // (for now, read cursor is always the start of buffer)
+    size_t minPDUSize = sizeof(Forte::PDU) - Forte::PDU::PDU_MAX_PAYLOAD;
+    if (mCursor < minPDUSize)
+    {
+        hlog(HLOG_DEBUG2, "buffer contains less data than minimum PDU");
+        return false;
+    }
+    Forte::PDU *pdu = reinterpret_cast<Forte::PDU *>(mPDUBuffer.get());
+    if (pdu->version != Forte::PDU::PDU_VERSION)
+    {
+        hlog(HLOG_DEBUG2, "invalid PDU version");
+        throw EPDUVersionInvalid();
+    }
+    // \TODO figure out how to do proper opcode validation
+
+    if (mCursor >= minPDUSize + pdu->payloadSize)
+        return true;
+    else
+        return false;
+}
+
+bool Forte::PDUPeer::RecvPDU(Forte::PDU &out)
+{
+    AutoUnlockMutex lock(mLock);
+    if (!lockedIsPDUReady())
+        return false;
+    size_t minPDUSize = sizeof(Forte::PDU) - Forte::PDU::PDU_MAX_PAYLOAD;
+    Forte::PDU *pdu = reinterpret_cast<Forte::PDU *>(mPDUBuffer.get());
+    size_t len = minPDUSize + pdu->payloadSize;
+    hlog(HLOG_DEBUG2, "found valid PDU: len=%llu", (u64) len);
+    // \TODO this memmove() based method is inefficient.  Implement a
+    // proper ring-buffer.
+    memcpy(&out, pdu, len);
+    // now, move the rest of the buffer and cursor back by 'len' bytes
+    memmove(mPDUBuffer.get(), mPDUBuffer.get() + len, mBufSize - len);
+    hlog(HLOG_DEBUG2, "found valid PDU: oldCursor=%llu newCursor=%llu", 
+         (u64) mCursor, (u64) mCursor - len);
+    mCursor -= len;
+    return true;
+    
+}
---
+++ unittest/PDUPeerUnitTest.cpp
@@ -0,0 +1,144 @@
+//#define BOOST_TEST_MODULE "PDUPeer Unit Tests"
+
+#include "boost/test/unit_test.hpp"
+#include <boost/shared_ptr.hpp>
+
+#include "AutoFD.h"
+#include "LogManager.h"
+#include "Context.h"
+#include "Foreach.h"
+#include "PDUPeer.h"
+#include "PDUPeerSet.h"
+
+using namespace boost::unit_test;
+using namespace boost;
+using namespace Forte;
+
+LogManager logManager;
+
+struct PDU_Test
+{
+    int a;
+    int b;
+    int c;
+    char d[128];
+} __attribute__((__packed__));
+
+void makeTestPDU(Forte::PDU &pdu, size_t &len)
+{
+    PDU_Test *testPDU = reinterpret_cast<PDU_Test*>(pdu.payload);
+    pdu.opcode = 1;
+    pdu.payloadSize = sizeof(PDU_Test);
+    testPDU->a = 1;
+    testPDU->b = 2;
+    testPDU->c = 3;
+    snprintf(testPDU->d, sizeof(testPDU->d), "111.222.333.444");
+    len = sizeof(Forte::PDU) - Forte::PDU::PDU_MAX_PAYLOAD;
+    len += pdu.payloadSize;
+}
+
+void receivePDU(void)
+{
+    Forte::PDU pdu;
+    size_t len;
+    makeTestPDU(pdu, len);
+
+    Forte::PDUPeer peer(0);
+    peer.DataIn(len, (char *)&pdu);
+    
+    Forte::PDU out;
+    bool received = peer.RecvPDU(out);
+    BOOST_CHECK_EQUAL(received, true);
+
+    int cmp = memcmp(&pdu, &out, len);
+    BOOST_CHECK_EQUAL(cmp, 0);
+}
+
+void receiveMultiplePDU(void)
+{
+    Forte::PDU pdu;
+    size_t len;
+    makeTestPDU(pdu, len);
+
+    Forte::PDUPeer peer(0);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        peer.DataIn(len, (char *)&pdu);
+    }    
+
+    Forte::PDU out;
+    for (int i = 0; i < 3; ++i)
+    {
+        bool received = peer.RecvPDU(out);
+        BOOST_CHECK_EQUAL(received, true);
+        int cmp = memcmp(&pdu, &out, len);
+        BOOST_CHECK_EQUAL(cmp, 0);
+    }
+}
+
+void pollPDU()
+{
+    try
+    {
+        int fds[2];
+        socketpair(AF_UNIX, SOCK_STREAM, 0, fds);
+        Forte::AutoFD fd1(fds[0]);
+        Forte::AutoFD fd2(fds[1]);
+        Forte::PDUPeerSet set1;
+        Forte::PDUPeerSet set2;
+
+        BOOST_CHECK_THROW(set1.Poll(), EPDUPeerSetNotPolling);
+
+        shared_ptr<PDUPeer> peer1 = set1.PeerCreate(fd1);
+        BOOST_CHECK(peer1);
+
+        set1.SetupEPoll();
+        set2.SetupEPoll();
+
+        shared_ptr<PDUPeer> peer2 = set2.PeerCreate(fd2);
+        BOOST_CHECK(peer2);
+
+        PDU pdu, rpdu;
+        size_t len;
+        makeTestPDU(pdu, len);
+
+        peer1->SendPDU(pdu);
+
+        set2.Poll(1000);
+
+        BOOST_CHECK(peer2->IsPDUReady());
+        BOOST_CHECK(peer2->RecvPDU(rpdu));
+        BOOST_CHECK(!memcmp(&pdu, &rpdu, sizeof(PDU)));
+    }
+    catch (Forte::Exception &e)
+    {
+        BOOST_FAIL(e.what().c_str());
+    }
+}
+
+BOOST_AUTO_TEST_CASE(receivePDUTest)
+{
+    receivePDU();
+}
+BOOST_AUTO_TEST_CASE(receiveMultiplePDUTest)
+{
+    receiveMultiplePDU();
+}
+
+BOOST_AUTO_TEST_CASE(pollUnitTest)
+{
+    pollPDU();
+}
+
+////Boost Unit init function ///////////////////////////////////////////////////
+test_suite*
+init_unit_test_suite(int argc, char* argv[])
+{
+
+    logManager.SetLogMask("//stdout", HLOG_ALL);
+    logManager.BeginLogging("//stdout");
+
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////
