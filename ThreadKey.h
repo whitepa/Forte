67ee84aaeb76df812a7c351e9b3beaa0687cd40b
Aniruddha Mulay <ani@scalecomputing.com>
2010-12-02 23:00:26 -0800

    porting Phil's cluster lock fix patch [https://crucible.scalecomputing.com/cru/CR-46] to Forte 2.0
    STOR-397

--- ThreadKey.h
+++ ThreadKey.h
@@ -1,37 +1,33 @@
 #ifndef __ThreadKey_h_
 #define __ThreadKey_h_
 
+#include <pthread.h>
 #include "Exception.h"
 
 namespace Forte{
 
-class ThreadKeyException : public Forte::Exception
-{
-public:
-    ThreadKeyException() {}
-    virtual ~ThreadKeyException() throw () {}
-};
+EXCEPTION_CLASS(EThreadKey);
+// -----------------------------------------------------------------------------
 
-class ThreadKey {
-public:
-    ThreadKey(void (*destructor)(void *) = NULL)
-    { pthread_key_create(&mKey, destructor); }
-    virtual ~ThreadKey()
-    { pthread_key_delete(mKey); }
+    class ThreadKey {
+    public:
+        ThreadKey(void (*destructor)(void *) = NULL)
+        { pthread_key_create(&mKey, destructor); }
+        virtual ~ThreadKey()
+        { pthread_key_delete(mKey); }
 
-private:
-    ThreadKey(const ThreadKey &other)
-        { throw ThreadKeyException(); }
-    const ThreadKey &operator=(const ThreadKey &rhs)
-        { throw ThreadKeyException(); return *this; }
+        void *Get(void)  __attribute__ ((no_instrument_function));
+        void Set(const void *value) __attribute__ ((no_instrument_function));
 
-public:
-    void *Get(void)  __attribute__ ((no_instrument_function));
-    void Set(const void *value) __attribute__ ((no_instrument_function));
+    protected:
+        pthread_key_t mKey;
 
-protected:
-    pthread_key_t mKey;
-};
+    private:
+        ThreadKey(const ThreadKey &other)
+            { throw EThreadKey(); }
+        const ThreadKey &operator=(const ThreadKey &rhs)
+            { throw EThreadKey(); return *this; }
+    };
 
 }; /* namespace Forte{} */
 // -----------------------------------------------------------------------------
