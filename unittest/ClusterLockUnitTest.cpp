9786e314e87eebc3fc0a58b4afa02a655faee50f
Phil White <phil@philwhite.org>
2011-01-30 04:39:47 -0800

    Forte ClusterLockUnitTest:  adding a multithreaded unit test to verify correct behavior in a MT environment

--- unittest/ClusterLockUnitTest.cpp
+++ unittest/ClusterLockUnitTest.cpp
@@ -1,11 +1,17 @@
-#include "LogManager.h"
+#include "Clock.h"
+#include "EventQueue.h"
 #include "FileSystem.h"
+#include "FTrace.h"
+#include "LogManager.h"
 #include "ProcRunner.h"
+#include "Thread.h"
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
 #include "ClusterLock.h"
+
+#include <boost/make_shared.hpp>
 
 using namespace Forte;
 using namespace boost;
@@ -154,16 +160,147 @@ TEST_F(ClusterLockTest, StaticMutexesAreFreed)
 {
     FString name1("StaticMutexesAreFreed1");
     FString name2("StaticMutexesAreFreed2");
-    ASSERT_EQ(0, ClusterLock::NumLocks());
+    ASSERT_EQ(0u, ClusterLock::NumLocks());
     {
         ClusterLock lock1(name1, 1);
-        ASSERT_EQ(1, ClusterLock::NumLocks());
+        ASSERT_EQ(1u, ClusterLock::NumLocks());
         {
             ClusterLock lock2(name2, 1);
-            ASSERT_EQ(2, ClusterLock::NumLocks());
+            ASSERT_EQ(2u, ClusterLock::NumLocks());
         }
-        ASSERT_EQ(1, ClusterLock::NumLocks());
+        ASSERT_EQ(1u, ClusterLock::NumLocks());
     }
-    ASSERT_EQ(0, ClusterLock::NumLocks());
+    ASSERT_EQ(0u, ClusterLock::NumLocks());
 }
 // -----------------------------------------------------------------------------
+
+/**
+ * LockThread is a thread which performs basic locking/unlocking
+ * actions at the will of a control thread.  The Lock/Unlock methods
+ * allow the control thread to request specific actions.
+ *
+ * Assumptions: The thread which calls the LockThread constructor will
+ * also call the destructor.  The same thread MUST also be the one
+ * which calls Wait().
+ */
+class LockThread : public Thread
+{
+public:
+    LockThread(const char *lockname)
+        : mLockName(lockname),
+          mHasLock(false),
+          mCompletionCond(mCompletionLock),
+          mHoldLock(mCompletionLock) {
+        initialized();
+    };
+    /**
+     * Lock() will notify this LockThread to attempt to obtain the
+     * named clusterlock.
+     */
+    void Lock(void) { FTRACE; mQueue.Add(make_shared<Event>("lock")); Notify(); }
+    /**
+     * Unlock() will notify this LockThread to unlock the named
+     * ClusterLock.
+     */
+    void Unlock(void) { FTRACE; mQueue.Add(make_shared<Event>("unlock")); Notify(); }
+    /**
+     * Wait() will not return until the last operation (Lock/Unlock)
+     * completes on this LockThread.  Wait() must be called for each
+     * operation (it operates completely synchronously).  The
+     * operation itself (Lock or Unlock) may complete prior to the
+     * Wait() call, but the LockThread will not continue processing
+     * the next operation until Wait() has been called.
+     */
+    void Wait(void) { FTRACE; mCompletionCond.Wait(); }
+
+    /**
+     * HasLock() will return whether this LockThread currently holds
+     * the named clusterlock.  HasLock() may be called at any time
+     * (including between a Lock/Unlock and its associated Wait()
+     * call).
+     */
+    bool HasLock(void) { return mHasLock; }
+protected:
+    virtual void * run(void) {
+        while (!mThreadShutdown)
+        {
+            shared_ptr<Event> e;
+            while (e = mQueue.Get())
+            {
+                if (e->mName == "lock")
+                {
+                    hlog(HLOG_DEBUG, "locking '%s'", mLockName.c_str());
+                    mLock.Lock(mLockName, 10);
+                    mHasLock = true;
+                }
+                else if (e->mName == "unlock")
+                {
+                    hlog(HLOG_DEBUG, "unlocking '%s'", mLockName.c_str());
+                    mLock.Unlock();
+                    mHasLock = false;
+                }
+                AutoUnlockMutex lock(mCompletionLock);
+                mCompletionCond.Signal();
+            }
+            interruptibleSleep(Timespec::FromSeconds(1));
+        }
+        return NULL;
+    }
+    FString mLockName;
+    ClusterLock mLock;
+    bool mHasLock;
+    EventQueue mQueue;
+
+    /**
+     * mCompletionLock guards the mCompletionCond, and is locked as
+     * soon as the thread starts up.
+     */
+    Mutex mCompletionLock;
+    /**
+     * mCompletionCond is Signal()led at the completion of each
+     * Lock/Unlock operation to allow an external control thread to
+     * synchronize on our state.
+     */
+    ThreadCondition mCompletionCond;
+    AutoUnlockMutex mHoldLock;
+};
+
+TEST_F(ClusterLockTest, MultiThreaded)
+{
+    FString name("MultiThreaded");
+    LockThread t1(name), t2(name);
+    ASSERT_EQ(false, t1.HasLock());
+    ASSERT_EQ(false, t2.HasLock());
+    ASSERT_EQ(0u, ClusterLock::NumLocks());
+    t1.Lock();
+    t1.Wait();
+    t2.Lock();
+    // there is currently no way to guarantee a wait until we know t2
+    // is blocked on the Lock().  Let's wait a bit:
+    usleep(100000);
+    ASSERT_EQ(true, t1.HasLock());
+    ASSERT_EQ(false, t2.HasLock());
+    ASSERT_EQ(1u, ClusterLock::NumLocks());
+    t1.Unlock();
+    t1.Wait();
+    t2.Wait();
+    ASSERT_EQ(false, t1.HasLock());
+    ASSERT_EQ(true, t2.HasLock());
+    ASSERT_EQ(1u, ClusterLock::NumLocks());
+    t1.Lock();
+    usleep(100000);
+    ASSERT_EQ(false, t1.HasLock());
+    ASSERT_EQ(true, t2.HasLock());
+    ASSERT_EQ(1u, ClusterLock::NumLocks());
+    t2.Unlock();
+    t2.Wait();
+    t1.Wait();
+    ASSERT_EQ(true, t1.HasLock());
+    ASSERT_EQ(false, t2.HasLock());
+    ASSERT_EQ(1u, ClusterLock::NumLocks());
+    t1.Unlock();
+    t1.Wait();
+    ASSERT_EQ(false, t1.HasLock());
+    ASSERT_EQ(false, t2.HasLock());
+    ASSERT_EQ(0u, ClusterLock::NumLocks());
+};
