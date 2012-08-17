082cbde14cd093a04fef81c628733d2897f31b61
hhsieh <hhsieh@scalecomputing.com>
2012-08-17 03:16:30 -0700

    [STOR-3151] pattern for abstracting processing bash commands
    Use a common design pattern for processing commands to
    faciliate code partitioning and unit testing.
    
    forte: Add ProcessCommand which is assembled from 2 templated parts:
           1) ProcessCommandRequest
              - assembles the process command from user defined fields
           2) ProcessCommandResponse
              - disseminates the process command response into user defined
                fields
    
           Add ProcessCommandUnitTest
    
    Change-Id: Iba8a7979127652b500211b5c76c32c0b194e1c7d
    Reviewed-on: http://gerrit.lab.local:8080/2401
    Tested-by: build
    Smoketests: build
    Reviewed-by: Hank Hsieh <hhsieh@scalecomputing.com>
    Reviewed-by: Chris Seawood <cseawood@scalecomputing.com>

---
+++ ProcessCommand.h
@@ -0,0 +1,210 @@
+#ifndef __ProcessCommand_H__
+#define __ProcessCommand_H__
+
+#include "FString.h"
+#include "ProcessManager.h"
+#include "LogManager.h"
+
+namespace Forte
+{
+
+/**
+    * @class ProcessCommand
+    *
+    * @brief This class encapsulates processing a process command and retrieving response
+    *
+    * template 'Request' can be derived from ProcessCommandRequest
+    * template 'Response' can be derived from ProcessCommandResponse
+    *
+    */
+template<class Request, class Response>
+class ProcessCommand
+{
+public:
+    typedef Request request_type;
+    typedef Response response_type;
+
+    /**
+      * execute the request and process the response
+      * @param pm  process manager
+      * @return  the response structure as defined by the 'Response' template
+      * @throws EProcessManager
+      */
+    virtual response_type operator()()
+    {
+        Forte::FString output;
+        hlogstream(HLOG_DEBUG, mRequest);
+        mProcessManager->CreateProcessAndGetResult(mRequest.AsString(), output);
+        response_type response(output);
+        hlogstream(HLOG_DEBUG, response);
+        return response;
+    }
+
+    explicit ProcessCommand(Forte::ProcessManagerPtr processManager)
+        :mProcessManager(processManager), mRequest()
+    {
+    }
+
+    template<typename A0>
+    explicit ProcessCommand(Forte::ProcessManagerPtr processManager,
+                            const A0& a0)
+        :mProcessManager(processManager), mRequest(a0)
+    {
+    }
+
+    template<typename A0, typename A1>
+    explicit ProcessCommand(Forte::ProcessManagerPtr processManager,
+                            const A0& a0, const A1& a1)
+        :mProcessManager(processManager), mRequest(a0, a1)
+    {
+    }
+
+    template<typename A0, typename A1, typename A2>
+    explicit ProcessCommand(Forte::ProcessManagerPtr processManager,
+                            const A0& a0, const A1& a1, const A2& a2)
+        :mProcessManager(processManager), mRequest(a0, a1, a2)
+    {
+    }
+
+    template<typename A0, typename A1, typename A2, typename A3>
+    explicit ProcessCommand(Forte::ProcessManagerPtr processManager,
+                            const A0& a0, const A1& a1, const A2& a2, const A3& a3)
+        :mProcessManager(processManager), mRequest(a0, a1, a2, a3)
+    {
+    }
+
+    template<typename A0, typename A1, typename A2, typename A3, typename A4>
+    explicit ProcessCommand(Forte::ProcessManagerPtr processManager,
+                            const A0& a0, const A1& a1, const A2& a2, const A3& a3, const A4& a4)
+        :mProcessManager(processManager), mRequest(a0, a1, a2, a3, a4)
+    {
+    }
+
+private:
+    Forte::ProcessManagerPtr mProcessManager;
+    const request_type mRequest;
+}; // ProcessCommand
+
+/**
+    * @class ProcessCommandData
+    *
+    * @brief This class encapsulates conversion to/from a process command
+    *
+    */
+class ProcessCommandData
+{
+public:
+    /**
+      * returns the string as would be seen executing a command in shell or it's return
+      */
+    virtual Forte::FString AsString() const=0;
+
+    virtual void PrintPretty(std::ostream& o) const
+    {
+        int status(-4);
+        char* res(abi::__cxa_demangle(typeid(*this).name(), NULL, NULL, &status));
+        if(status == 0)
+        {
+            o << res;
+        }
+        o << ": '" << AsString() << "'";
+        free(res);
+    }
+};
+
+/**
+    * @class ProcessCommandRequest
+    *
+    * @brief This class encapsulates/facilitates the conversion to shell from user format
+    */
+class ProcessCommandRequest : public ProcessCommandData
+{
+public:
+    virtual ~ProcessCommandRequest(){}
+};
+
+/**
+    * @class ProcessCommandRequestBasic
+    *
+    * @brief Basic implementation of ProcessCommandRequest which just passes
+    *        the string argument
+    */
+class ProcessCommandRequestBasic : public ProcessCommandRequest
+{
+public:
+    explicit ProcessCommandRequestBasic(const Forte::FString& request)
+        :mRequest(request)
+    {
+    }
+
+    virtual Forte::FString AsString() const
+    {
+        return mRequest;
+    }
+
+protected:
+    const Forte::FString mRequest;
+};
+
+/**
+    * @class ProcessCommandResponse
+    *
+    * @brief This class encapsulates/facilitates the conversion from shell response
+    *        to user format
+    */
+class ProcessCommandResponse : public ProcessCommandData
+{
+public:
+    virtual ~ProcessCommandResponse(){}
+
+    virtual Forte::FString AsString() const
+    {
+        return mResponse;
+    }
+
+protected:
+    explicit ProcessCommandResponse(const Forte::FString& response)
+        :mResponse(response)
+    {
+    }
+
+protected:
+    const Forte::FString mResponse;
+};
+
+/**
+    * @class ProcessCommandResponseBasic
+    *
+    * @brief Basic implementation of ProcessCommandResponse which just passes
+    *        the string argument to ctor
+    */
+class ProcessCommandResponseBasic : public ProcessCommandResponse
+{
+public:
+    explicit ProcessCommandResponseBasic(const Forte::FString& response)
+        :ProcessCommandResponse(response)
+    {
+    }
+};
+
+inline std::ostream& operator<<(std::ostream& out, const Forte::ProcessCommandData& obj)
+{
+    obj.PrintPretty(out);
+    return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const Forte::ProcessCommandRequest& obj)
+{
+    obj.PrintPretty(out);
+    return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const Forte::ProcessCommandResponse& obj)
+{
+    obj.PrintPretty(out);
+    return out;
+}
+
+} // namespace Forte
+
+#endif // __ProcessCommand_H__
---
+++ unittest/ProcessCommandUnitTest.cpp
@@ -0,0 +1,120 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "LogManager.h"
+#include "ProcessCommand.h"
+#include "GMockProcessManager.h"
+
+using namespace Forte;
+using namespace boost;
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::ReturnArg;
+using ::testing::ReturnRef;
+using ::testing::AnyNumber;
+using ::testing::StrEq;
+using ::testing::SetArgReferee;
+using ::testing::ContainsRegex;
+using ::testing::Invoke;
+using ::testing::InSequence;
+using ::testing::Mock;
+
+
+LogManager logManager;
+
+class ProcessCommandTest : public ::testing::Test
+{
+public:
+    static void SetUpTestCase() {
+        logManager.SetLogMask("//stdout", HLOG_ALL);
+        logManager.BeginLogging("//stdout");
+    };
+    static void TearDownTestCase() {
+    };
+
+};
+
+TEST_F(ProcessCommandTest, basic)
+{
+    typedef Forte::ProcessCommand<Forte::ProcessCommandRequestBasic, Forte::ProcessCommandResponseBasic>
+            ProcessCommandEcho;
+
+    boost::shared_ptr<GMockProcessManager> pm(new GMockProcessManager);
+
+    static const char* ret("\n");
+    EXPECT_CALL(*pm, CreateProcessAndGetResult(
+                    ::testing::StrEq("echo"),
+                    ::testing::_,
+                    ::testing::_,
+                    ::testing::_,
+                    ::testing::_,
+                    ::testing::_))
+        .Times(1)
+        .WillOnce(DoAll(::testing::SetArgReferee<1>(ret),
+                      ::testing::Return(0)));
+
+    ProcessCommandEcho echo(pm, "echo");
+    ProcessCommandEcho::response_type response(echo());
+    EXPECT_EQ("\n", response.AsString());
+}
+
+
+class LSProcessCommandRequest : public Forte::ProcessCommandRequest
+{
+public:
+    LSProcessCommandRequest(const Forte::FString& path)
+        :mLs("ls"), mRequest(mLs + " " + path)
+    {
+    }
+
+    Forte::FString AsString() const
+    {
+        return mRequest;
+    }
+
+private:
+    const Forte::FString mLs;
+    const Forte::FString mRequest;
+};
+
+class LSProcessCommandResponse : public ProcessCommandResponse
+{
+public:
+    LSProcessCommandResponse(const Forte::FString& output)
+        :ProcessCommandResponse(output)
+    {
+    }
+
+    std::vector<FString> GetFileList() const
+    {
+        std::vector<FString> components;
+        mResponse.Explode(" ", components);
+        return components;
+    }
+};
+
+TEST_F(ProcessCommandTest, ls)
+{
+    typedef ProcessCommand<LSProcessCommandRequest, LSProcessCommandResponse> LSProcessCommand;
+
+    boost::shared_ptr<GMockProcessManager> pm(new GMockProcessManager);
+
+    static const char* ret("a b c d e f g");
+
+    EXPECT_CALL(*pm, CreateProcessAndGetResult(
+                    ::testing::StrEq("ls /tmp"),
+                    ::testing::_,
+                    ::testing::_,
+                    ::testing::_,
+                    ::testing::_,
+                    ::testing::_))
+        .Times(1)
+        .WillOnce(DoAll(::testing::SetArgReferee<1>(ret),
+                      ::testing::Return(0)));
+
+    LSProcessCommand qemuInfo(pm, "/tmp");
+    LSProcessCommand::response_type response(qemuInfo());
+    EXPECT_EQ(7, response.GetFileList().size());
+    EXPECT_EQ("a", response.GetFileList()[0]);
+    EXPECT_EQ("g", response.GetFileList()[6]);
+}
