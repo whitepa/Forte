20e09d77ce25de2a16e37721af6e7af15160b0c3
Nate Hardt <nate@scalecomputing.com>
2012-09-12 17:46:46 -0700

    forte2: whitespace and formatting
    removed whitespace
    applied more recent standards of spacing
    removed stale comments
    
    Change-Id: I1be769b448847650f23248a22ae4de7b2bfd655f
    Reviewed-on: http://gerrit.lab.local:8080/2381
    Tested-by: build
    Smoketests: build
    Reviewed-by: Nate Hardt <nate@scalecomputing.com>
    Reviewed-by: Tim Jasko <tjasko@scalecomputing.com>
    Reviewed-by: Chris Seawood <cseawood@scalecomputing.com>

--- ReceiverThread.cpp
+++ ReceiverThread.cpp
@@ -8,20 +8,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <boost/make_shared.hpp>
-//#include <netinet/ip.h>
-
-// \TODO remove this once EPOLLRDHUP makes it into sys/epoll.h
-#ifndef EPOLLRDHUP
-#define EPOLLRDHUP 0x2000
-#endif
 
 #define EPOLLTIMEOUT 500
-
 
 using namespace Forte;
 
 // this thread just loops and accepts connections
-
 void * Forte::ReceiverThread::run(void)
 {
     // init thread name
@@ -30,8 +22,9 @@ void * Forte::ReceiverThread::run(void)
     // create socket
     AutoFD m;
     if ((m = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-        throw EReceiverThread(FStringFC(), "Failed to create socket: %s", strerror(errno));
-    
+        throw EReceiverThread(
+            FStringFC(), "Failed to create socket: %s", strerror(errno));
+
     // set SO_REUSEADDR
     int one = 1;
     setsockopt(m, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
@@ -45,13 +38,15 @@ void * Forte::ReceiverThread::run(void)
     bind_addr.sin_family = AF_INET;
     bind_addr.sin_port = htons(mPort);
     if (!inet_aton(mBindIP, &(bind_addr.sin_addr)))
-        throw EReceiverThread(FStringFC(), "invalid bind IP: %s", mBindIP.c_str());
+        throw EReceiverThread(
+            FStringFC(), "invalid bind IP: %s", mBindIP.c_str());
 
-    if (::bind(m, (struct sockaddr *)&bind_addr, sizeof(struct sockaddr_in))==-1)
+    if (::bind(m, (struct sockaddr*)&bind_addr, sizeof(struct sockaddr_in)) == -1)
         throw EReceiverThread(FStringFC(), "failed to bind: %s", strerror(errno));
 
-    if (listen(m, mBacklog)==-1)
-        throw EReceiverThread(FStringFC(), "failed to listen: %s", strerror(errno));
+    if (listen(m, mBacklog) == -1)
+        throw EReceiverThread(
+            FStringFC(), "failed to listen: %s", strerror(errno));
 
     // set socket to close-on-exec, which prevents a bug wherein a
     // child process that restarts this server will still have this
@@ -87,19 +82,19 @@ void * Forte::ReceiverThread::run(void)
         int s;
         struct epoll_event events[1];
 
-        int epollResult = epoll_wait(efd,events,1,EPOLLTIMEOUT);
+        int epollResult = epoll_wait(efd, events, 1, EPOLLTIMEOUT);
 
         if (epollResult == 0)
         {
-                continue;
+            continue;
         }
         else
         {
             if (epollResult < 0 &&
-                    ((errno == EBADF ||
-                      errno == EFAULT ||
-                      errno == EINVAL) &&
-                     errno != EINTR))
+                ((errno == EBADF ||
+                  errno == EFAULT ||
+                  errno == EINVAL) &&
+                 errno != EINTR))
             {
                 throw EReceiverThreadPollFailed(strerror(errno));
             }
@@ -118,14 +113,12 @@ void * Forte::ReceiverThread::run(void)
             continue;
         }
 
-        // TODO: shared_ptr
         boost::shared_ptr<RequestEvent> e = make_shared<RequestEvent>();
         e->mFD = s;
         // mark the time of accept
         gettimeofday(&(e->mTime), NULL);
         // TODO: store client IP address in mClient
-        
-        // queue the event
+
         mDisp->Enqueue(e);
     }
     return NULL;
--- ReceiverThread.h
+++ ReceiverThread.h
@@ -9,31 +9,56 @@
 namespace Forte
 {
     EXCEPTION_CLASS(EReceiverThread);
-    EXCEPTION_SUBCLASS(EReceiverThread, EReceiverThreadNameInvalid);
-    EXCEPTION_SUBCLASS2(EReceiverThread, EReceiverDispatcherInvalid, "Dispatcher is invalid");
-    EXCEPTION_SUBCLASS2(EReceiverThread, ERecieverThreadPollAdd, "Failed to add descriptor to poll socket");
-    EXCEPTION_SUBCLASS2(EReceiverThread, EReceiverThreadPollCreate, "Failed to create poll socket");
-    EXCEPTION_SUBCLASS(EReceiverThread,EReceiverThreadPollFailed);
+
+    EXCEPTION_SUBCLASS(EReceiverThread,
+                       EReceiverThreadNameInvalid);
+
+    EXCEPTION_SUBCLASS2(EReceiverThread,
+                        EReceiverDispatcherInvalid,
+                        "Dispatcher is invalid");
+
+    EXCEPTION_SUBCLASS2(EReceiverThread,
+                        ERecieverThreadPollAdd,
+                        "Failed to add descriptor to poll socket");
+
+    EXCEPTION_SUBCLASS2(EReceiverThread,
+                        EReceiverThreadPollCreate,
+                        "Failed to create poll socket");
+
+    EXCEPTION_SUBCLASS(EReceiverThread, EReceiverThreadPollFailed);
+
     class ReceiverThread : public Thread
     {
     public:
-        inline ReceiverThread(shared_ptr<Dispatcher> disp, const char *name, 
-                              int port, int backlog, const char *bindIP = "") :
-            mDisp(disp), mName(name), 
-            mPort(port), mBacklog(backlog), mBindIP(bindIP)
+        inline ReceiverThread(
+            boost::shared_ptr<Dispatcher> disp,
+            const char *name,
+            int port,
+            int backlog,
+            const char *bindIP = "")
+            :  mDisp(disp),
+               mName(name),
+               mPort(port),
+               mBacklog(backlog),
+               mBindIP(bindIP)
             {
-                if (!disp) throw EReceiverDispatcherInvalid();
-                if (mName.empty()) throw Exception("receiver must be given a valid name");
+                if (!disp)
+                    throw EReceiverDispatcherInvalid();
+
+                if (mName.empty())
+                    throw Exception("receiver must be given a valid name");
+
                 // TODO: validate IP address
                 initialized();
             }
-        virtual ~ReceiverThread() { 
+
+        virtual ~ReceiverThread() {
             deleting();
         }
 
     protected:
         virtual void * run();
-    
+
         boost::shared_ptr<Dispatcher> mDisp;
         FString mName;
         int mPort;
