fe3d4b4a473b306b0d6a994566ca5a0b2da14267
Asad Saeed <asaeed@scalecomputing.com>
2012-09-28 12:33:47 -0700

    Forte::ContextPredicate: add easier op support
    ContextPredicate:
      Added namespace ContextPredicateOp with
          And, Or, Not functions
      Made ContextPredicate noncopyable.
      Remove implicit cast to SharedPtr
      Utilize shared_from_this when Inverting
      Add Evaluate with timeout capability
    
    ContextPredicateFunction:
      Changed return type to match Condition::DecisionResult
          This allows state machine transitions to be more valid
          when the PredicateFunction relies on checkedvalues.
      Modified all users of ContextPredicateFunction
        GrubManager: Use checkedvalues time
        DriveManager: Use Current Monotonic Time
    
    Change-Id: Ic457baf07b3b9bdfa8110b9c9d8189d66c3ffb2d
    Reviewed-on: http://gerrit.lab.local:8080/2521
    Tested-by: build
    Smoketests: build
    Reviewed-by: Asad Saeed <asaeed@scalecomputing.com>
    Reviewed-by: Jyotindra Vasudeo <jvasudeo@scalecomputing.com>

--- unittest/ContextPredicateUnitTest.cpp
+++ unittest/ContextPredicateUnitTest.cpp
@@ -28,12 +28,16 @@ public:
 
     };
 
-    static bool PredicateFunctionSet(const Forte::Context &c) {
-        return true;
+    static ContextPredicateFunctionResult PredicateFunctionSet(
+            const Forte::Context &c)
+    {
+        return make_tuple(true, MonotonicClock().GetTime());
     }
 
-    static bool PredicateFunctionClear(const Forte::Context &c) {
-        return false;
+    static ContextPredicateFunctionResult PredicateFunctionClear(
+            const Forte::Context &c)
+    {
+        return make_tuple(false, MonotonicClock().GetTime());
     }
 };
 
@@ -44,69 +48,72 @@ TEST_F (ContextPredicateTest, s_test1)
     c2.Set("string", ObjectPtr(new CheckedStringEnum(validStrArray)));
     c1.Get<CheckedStringEnum>("string")->Set("One");
     c2.Get<CheckedStringEnum>("string")->Set("Two");
-    Forte::ContextPredicate cp1(Forte::ContextPredicate::CHECKED_STRING_EQUALS,
+    Forte::ContextPredicatePtr cp1 = make_shared<ContextPredicate>(Forte::ContextPredicate::CHECKED_STRING_EQUALS,
                                "string", "One");    
 
-    EXPECT_TRUE( cp1.Evaluate(c1) );
-    EXPECT_FALSE( cp1.Evaluate(c2) );
+    EXPECT_TRUE( cp1->Evaluate(c1) );
+    EXPECT_FALSE( cp1->Evaluate(c2) );
 
     // test int equality
     c1.Set("int1", ObjectPtr(new CheckedInt32()));
     c2.Set("int1", ObjectPtr(new CheckedInt32()));
     c1.Get<CheckedInt32>("int1")->Set(1);
     c2.Get<CheckedInt32>("int1")->Set(2);
-    Forte::ContextPredicate cp2(Forte::ContextPredicate::CHECKED_INT_EQUALS,
+    Forte::ContextPredicatePtr cp2 = make_shared<ContextPredicate>(Forte::ContextPredicate::CHECKED_INT_EQUALS,
                                 "int1", 1);
-    EXPECT_TRUE( cp2.Evaluate(c1) );
-    EXPECT_FALSE( cp2.Evaluate(c2) );
+    EXPECT_TRUE( cp2->Evaluate(c1) );
+    EXPECT_FALSE( cp2->Evaluate(c2) );
 
     // test int GTE
-    Forte::ContextPredicate cp3(Forte::ContextPredicate::CHECKED_INT_GTE,
+    Forte::ContextPredicatePtr cp3 = make_shared<ContextPredicate>(Forte::ContextPredicate::CHECKED_INT_GTE,
                                 "int1", 2);
-    Forte::ContextPredicate cp4(Forte::ContextPredicate::CHECKED_INT_GTE,
+    Forte::ContextPredicatePtr cp4 = make_shared<ContextPredicate>(Forte::ContextPredicate::CHECKED_INT_GTE,
                                 "int1", 0);
     c1.Get<CheckedInt32>("int1")->Set(1);
-    EXPECT_FALSE( cp3.Evaluate(c1) );
-    EXPECT_TRUE( cp3.Evaluate(c2) );
-    EXPECT_TRUE( cp4.Evaluate(c1) );
-    EXPECT_TRUE( cp4.Evaluate(c2) );
+    EXPECT_FALSE( cp3->Evaluate(c1) );
+    EXPECT_TRUE( cp3->Evaluate(c2) );
+    EXPECT_TRUE( cp4->Evaluate(c1) );
+    EXPECT_TRUE( cp4->Evaluate(c2) );
 
     // test exception from checked int (uninitialized)
     c1.Set("int2", ObjectPtr(new CheckedInt32()));
-    Forte::ContextPredicate cp5(Forte::ContextPredicate::CHECKED_INT_GTE,
+    Forte::ContextPredicatePtr cp5 = make_shared<ContextPredicate>(Forte::ContextPredicate::CHECKED_INT_GTE,
                                 "int2", 0);
-    EXPECT_THROW( cp5.Evaluate(c1, Timespec::FromSeconds(-1)),
+    EXPECT_THROW( cp5->Evaluate(c1, Timespec::FromSeconds(-1)),
                   ECheckedValueUninitialized );
 
     c1.Get<CheckedInt32>("int2")->Set(0);
-    EXPECT_TRUE( cp5.Evaluate(c1) );
+    EXPECT_TRUE( cp5->Evaluate(c1) );
 
     // test invalid predicate exception
-    Forte::ContextPredicate cp6(Forte::ContextPredicate::NOT, cp4, cp5);
-    EXPECT_THROW( cp6.Evaluate(c1), EContextPredicateInvalid );
+    Forte::ContextPredicatePtr cp6 = make_shared<ContextPredicate>(
+            Forte::ContextPredicate::NOT, cp4, cp5);
+    EXPECT_THROW( cp6->Evaluate(c1), EContextPredicateInvalid );
 
     // test predicate function
-    Forte::ContextPredicate cp7(Forte::ContextPredicate::PREDICATE_FUNCTION_SET, 
+    Forte::ContextPredicatePtr cp7 = make_shared<ContextPredicate>(
+            Forte::ContextPredicate::PREDICATE_FUNCTION_SET,
                                 boost::make_shared<Forte::ContextPredicateFunction>(
                                     ContextPredicateTest::PredicateFunctionSet));
-    EXPECT_TRUE( cp7.Evaluate(c1) );
+    EXPECT_TRUE( cp7->Evaluate(c1) );
 
-    Forte::ContextPredicate cp8(Forte::ContextPredicate::PREDICATE_FUNCTION_CLEAR, 
+    Forte::ContextPredicatePtr cp8 = make_shared<ContextPredicate>(
+            Forte::ContextPredicate::PREDICATE_FUNCTION_CLEAR,
                                 boost::make_shared<Forte::ContextPredicateFunction>(
                                     ContextPredicateTest::PredicateFunctionClear));
-    EXPECT_TRUE( cp8.Evaluate(c1) );
+    EXPECT_TRUE( cp8->Evaluate(c1) );
 
     // test AND
-    EXPECT_TRUE(cp1.Evaluate(c1));
-    EXPECT_TRUE(cp2.Evaluate(c1));
-    EXPECT_TRUE(cp4.Evaluate(c1));
+    EXPECT_TRUE(cp1->Evaluate(c1));
+    EXPECT_TRUE(cp2->Evaluate(c1));
+    EXPECT_TRUE(cp4->Evaluate(c1));
     EXPECT_TRUE(
         Forte::ContextPredicate(
             Forte::ContextPredicate::AND, cp1, cp2, cp4)
         .Evaluate(c1) == true);
 
     c2.Get<CheckedInt32>("int1")->Set(1);
-    EXPECT_TRUE(cp2.Evaluate(c2));
+    EXPECT_TRUE(cp2->Evaluate(c2));
     EXPECT_FALSE(
         Forte::ContextPredicate(
             Forte::ContextPredicate::AND, cp1, cp2, cp4)
@@ -123,7 +130,7 @@ TEST_F (ContextPredicateTest, s_test1)
         .Evaluate(c2) == false);
 
     // test NOT
-    EXPECT_TRUE(cp1.Evaluate(c1));
+    EXPECT_TRUE(cp1->Evaluate(c1));
     EXPECT_FALSE(
         Forte::ContextPredicate(
             Forte::ContextPredicate::NOT, cp1)
