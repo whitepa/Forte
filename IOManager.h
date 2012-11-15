16baa81adfad207ea097ed94b08c53d62f1fbd7d
Phil White <phil@philwhite.org>
2012-11-15 15:14:15 -0800

    [STOR-3622] Forte IOManager
    Change-Id: Ife56c8959de4fff0782879a96d3ad1bdfedfd2b8

---
+++ IOManager.h
@@ -0,0 +1,214 @@
+#ifndef __IORequest_h__
+#define __IORequest_h__
+
+#include "SystemCallUtil.h"
+#include "Thread.h"
+#include "LogManager.h"
+#include "FTrace.h"
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+namespace Forte
+{
+    EXCEPTION_CLASS(EIOManager);
+    EXCEPTION_SUBCLASS2(EIOManager, EIOSubmitFailed,
+                        "Failed to submit IO operation");
+
+    EXCEPTION_SUBCLASS(EIOManager, EIORequest);
+    EXCEPTION_SUBCLASS2(EIORequest, EIORequestStillInProgress,
+                        "The IO request is still in progress");
+    class IORequest
+    {
+    public:
+        typedef enum {
+            READ,
+            WRITE
+        } OperationType;
+        typedef boost::function<void(const IORequest &req)> IOCompletionCallback;
+
+        IORequest() :
+//            mIOManager(ioManager),
+            mCompleted(false), mResult(0),mData(NULL) {
+            memset(&mIOCB, 0, sizeof(struct iocb));
+            mIOCBs[0] = &mIOCB;
+        }
+        void SetOp(IORequest::OperationType op) {
+            switch (op) {
+            case READ:
+                mIOCB.aio_lio_opcode = IO_CMD_PREAD;
+                break;
+            case WRITE:
+                mIOCB.aio_lio_opcode = IO_CMD_PWRITE;
+                break;
+            default:
+                break;
+            }
+        }
+        void SetBuffer(void *buf, size_t len) {
+            FTRACE2("buf=%p len=%lu", buf, len);
+            mIOCB.u.c.buf = buf;
+            mIOCB.u.c.nbytes = len;
+        }
+        void SetOffset(off_t off) {
+            mIOCB.u.c.offset = off;
+        }
+        void SetFD(int fd) {
+            mIOCB.aio_fildes = fd;
+        }
+        void SetCallback(IOCompletionCallback cb) {
+            mCallback = cb;
+        }
+        void SetUserData(void *data) {
+            mData = data;
+        }
+        void * GetUserData(void) const {
+            return mData;
+        }
+        // Called only by IOManager
+        void SetRequestNumber(uint64_t reqNum) {
+            mIOCB.data = (void *)reqNum;
+        }
+
+        uint64_t GetRequestNumber(void) const {
+            return (uint64_t)mIOCB.data;
+        }
+
+        struct iocb ** GetIOCBs(void) {
+            return mIOCBs;
+        }
+        void SetResult(long res) {
+            FTRACE2("res=%ld", res);
+            mResult = res;
+            mCompleted = true;
+            if (mCallback)
+                mCallback(*this);
+        }
+        long GetResult(void) const {
+            if (!mCompleted)
+                throw EIORequestStillInProgress();
+            return mResult;
+        }
+
+        // void WaitForCompletion(void);
+        bool IsComplete(void) const {
+            return mCompleted;
+        }
+
+        struct iocb mIOCB;
+        struct iocb *mIOCBs[1];
+        IOCompletionCallback mCallback;
+        bool mCompleted;
+        long mResult;
+        void *mData;
+    };
+
+
+    typedef boost::shared_ptr<IORequest> IORequestPtr;
+    typedef std::pair<uint64_t, IORequestPtr> IORequestPair;
+    typedef std::map<uint64_t, IORequestPtr> RequestMap;
+
+    class IOManager : public Forte::Thread
+    {
+    public:
+        IOManager(int maxRequests) :
+            mIOContext(NULL),
+            mRequestCounter(0) {
+            int err;
+            // @TODO check range on maxRequests
+            if ((err = io_setup(maxRequests, &mIOContext)) != 0)
+                SystemCallUtil::ThrowErrNoException(-err);
+//            mIOCB = new struct iocb[maxRequests];
+            initialized();
+        }
+        virtual ~IOManager() {
+            // @TODO Wait for all IO completion
+            deleting();
+            int err;
+            if ((err = io_destroy(mIOContext)) != 0)
+                hlog(HLOG_ERR, "io_destroy: %s", strerror(-err));
+        }
+        virtual void * run(void) {
+            FTRACE;
+            struct timespec timeout = Timespec::FromMillisec(200);
+            struct io_event events[32];
+            while (!IsShuttingDown()) {
+                // @TODO correctly handle shutdown with pending requests
+                int n = 0;
+                memset(events, 0, sizeof(struct io_event)*32);
+                n = io_getevents(mIOContext, 1, 32, events, &timeout);
+                if (n > 0)
+                {
+                    hlog(HLOG_DEBUG, "io_getevents returns %d events", n);
+                }
+                else if (n < 0)
+                {
+                    hlog(HLOG_ERR, "io_getevents returns %d: %s", n, strerror(-n));
+                    InterruptibleSleep(Timespec::FromMillisec(200));
+                    continue;
+                }
+                for (int i = 0; i < n; ++i)
+                {
+                    hlog(HLOG_DEBUG, "received IO completion for request %lu",
+                         (long)events[i].obj->data);
+                    HandleCompletionEvent(&events[i]);
+                }
+            }
+            return NULL;
+        }
+
+        boost::shared_ptr<IORequest> NewRequest() {
+            // @TODO allocate from a pool
+            return boost::make_shared<IORequest>();
+        }
+
+        uint64_t SubmitRequest(const boost::shared_ptr<IORequest> &req) {
+            // take ownership of request
+            AutoUnlockMutex lock(mLock);
+            req->SetRequestNumber(mRequestCounter);
+            int res;
+            while ((res = io_submit(mIOContext, 1, req->GetIOCBs())) < 0)
+            {
+                if (-res == EAGAIN)
+                    continue;
+                SystemCallUtil::ThrowErrNoException(-res);
+            }
+            if (res == 0)
+                throw EIOSubmitFailed();
+            mPendingRequests[mRequestCounter] = req;
+            return mRequestCounter++;
+        }
+
+        void HandleCompletionEvent(const struct io_event *event) {
+            uint64_t requestNum = (uint64_t)event->obj->data;
+            boost::shared_ptr<IORequest> req;
+            {
+                AutoUnlockMutex lock(mLock);
+                RequestMap::iterator i = mPendingRequests.find(requestNum);
+                if (i == mPendingRequests.end())
+                {
+                    hlog(HLOG_ERR, "ignoring IO completion event for unknown request %lu",
+                         requestNum);
+                    return;
+                }
+                req = (*i).second;
+                mPendingRequests.erase(requestNum);
+            }
+            // Set result, triggering callbacks (from this thread)
+            req->SetResult(event->res);
+        }
+
+        Forte::Mutex mLock;
+
+        io_context_t mIOContext;
+
+        // Counter for submitted requests
+        uint64_t mRequestCounter;
+
+        // map of request number to request
+        RequestMap mPendingRequests;
+    };
+
+}
+
+#endif
---
+++ unittest/IOManagerUnitTest.cpp
@@ -0,0 +1,407 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <libaio.h>
+#include "LogManager.h"
+#include "FTrace.h"
+#include "FileSystemImpl.h"
+#include "IOManager.h"
+
+using namespace Forte;
+
+LogManager logManager;
+
+class IOManagerUnitTest : public ::testing::Test
+{
+public:
+    IOManagerUnitTest() :
+        mCond(mLock),mNumCompletions(0)
+    {
+        hlog(HLOG_INFO, "initializing test case");
+        mTmpfile = mFileSystem.MakeTemporaryFile("IOManagerUnitTest-XXXXXX");
+//        mTmpfile = "tmpfile";
+    }
+    ~IOManagerUnitTest() {
+        mFileSystem.Unlink(mTmpfile);
+    }
+
+    static void SetUpTestCase() {
+        logManager.BeginLogging("//stderr");
+    }
+    static void TearDownTestCase() {
+        logManager.EndLogging();
+    }
+
+    static void Notify(const IORequest &req) {
+        FTRACE;
+        hlog(HLOG_INFO, "IO request %lu completed, result is %ld",
+             req.GetRequestNumber(), req.GetResult());
+        IOManagerUnitTest &ut(*(reinterpret_cast<IOManagerUnitTest*>(req.GetUserData())));
+        AutoUnlockMutex lock(ut.mLock);
+        ++ut.mNumCompletions;
+        ut.mCond.Signal();
+    }
+    void Wait(const int numCompletions = 1) {
+        AutoUnlockMutex lock(mLock);
+        while (mNumCompletions < numCompletions)
+            mCond.Wait();
+    }
+    Mutex mLock;
+    ThreadCondition mCond;
+    int mNumCompletions;
+    FileSystemImpl mFileSystem;
+    FString mTmpfile;
+};
+
+TEST_F(IOManagerUnitTest, DirectIOFileOpen)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    system(FString("/bin/dd bs=1024 count=10000 if=/dev/zero of=" + mTmpfile));
+
+    // Open the file with direct IO
+    AutoFD fd(open(mTmpfile, O_DIRECT));
+    ASSERT_NE(fd.GetFD(), -1);
+}
+
+TEST_F(IOManagerUnitTest, DirectIOFileReadAligned)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // populate the file with some data
+    system(FString("/bin/dd bs=1024 count=10000 if=/dev/zero of=" + mTmpfile));
+
+    // Open the file with direct IO
+    AutoFD fd(open(mTmpfile, O_DIRECT));
+    ASSERT_NE(fd.GetFD(), -1);
+
+    // Allocate a buffer
+    char *buf = 0;
+    size_t len = 4096;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/len));
+    memset(buf, 0, len);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+    req->SetOp(IORequest::READ);
+    req->SetCallback(IOManagerUnitTest::Notify);
+    req->SetUserData(this);
+    req->SetBuffer(buf, len);
+    req->SetOffset(4096);
+    req->SetFD(fd);
+    uint64_t reqnum = iomgr->SubmitRequest(req);
+    hlog(HLOG_INFO, "submitted IO request %lu", reqnum);
+    Wait();
+    hlog(HLOG_INFO, "got result %ld", req->GetResult());
+    ASSERT_EQ(len, req->GetResult());
+    free(buf);
+}
+TEST_F(IOManagerUnitTest, DirectIOFileReadUnaligned)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // populate the file with some data
+    system(FString("/bin/dd bs=1024 count=10000 if=/dev/zero of=" + mTmpfile));
+
+    // Open the file with direct IO
+    AutoFD fd(open(mTmpfile, O_DIRECT));
+    ASSERT_NE(fd.GetFD(), -1);
+
+    // Allocate a buffer
+    char *buf = 0;
+    size_t len = 1024;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/4096));
+    memset(buf, 0, len);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+    req->SetOp(IORequest::READ);
+    req->SetCallback(IOManagerUnitTest::Notify);
+    req->SetUserData(this);
+    req->SetBuffer(buf+128, 512); // misaligned
+    req->SetOffset(4096);
+    req->SetFD(fd);
+    uint64_t reqnum = iomgr->SubmitRequest(req);
+    hlog(HLOG_INFO, "submitted IO request %lu", reqnum);
+    Wait();
+    hlog(HLOG_INFO, "got result %ld", req->GetResult());
+    if (req->GetResult() < 0)
+        hlog(HLOG_INFO, "error is %s", strerror(-req->GetResult()));
+    ASSERT_EQ(-22, req->GetResult());
+    free(buf);
+}
+
+TEST_F(IOManagerUnitTest, DirectIOFileWriteAligned)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // Open the file with direct IO
+    int fd(open(mTmpfile, O_RDWR | O_CREAT | O_DIRECT));
+    ASSERT_NE(fd, -1);
+
+    // Allocate a buffer
+    char *buf = 0;
+    size_t len = 4096;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/len));
+    memset(buf, 65, len);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+    req->SetOp(IORequest::WRITE);
+    req->SetBuffer(buf, len);
+    req->SetOffset(0);
+    req->SetFD(fd);
+    req->SetCallback(IOManagerUnitTest::Notify);
+    req->SetUserData(this);
+    hlog(HLOG_INFO, "got here");
+    uint64_t reqnum = iomgr->SubmitRequest(req);
+    hlog(HLOG_INFO, "submitted IO request %lu", reqnum);
+    Wait();
+    ASSERT_EQ(4096, req->GetResult());
+    free(buf);
+}
+
+TEST_F(IOManagerUnitTest, DirectIOFileWriteUnaligned)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // Open the file with direct IO
+    int fd(open(mTmpfile, O_RDWR | O_CREAT | O_DIRECT));
+    ASSERT_NE(fd, -1);
+
+    // Allocate a buffer
+    char *buf = 0;
+    ssize_t len = 1024;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/4096));
+    off_t interval = 64;
+    int i = 0;
+    for (off_t offset = 0; offset < 4096; offset += interval)
+        memset(buf + offset, ++i, interval);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+    req->SetOp(IORequest::WRITE);
+    req->SetBuffer(buf+128, len); // unaligned
+    req->SetOffset(0);
+    req->SetFD(fd);
+    req->SetCallback(IOManagerUnitTest::Notify);
+    req->SetUserData(this);
+    hlog(HLOG_INFO, "got here");
+    uint64_t reqnum = iomgr->SubmitRequest(req);
+    hlog(HLOG_INFO, "submitted IO request %lu", reqnum);
+    Wait();
+    hlog(HLOG_INFO, "got result %ld", req->GetResult());
+    if (req->GetResult() < 0)
+        hlog(HLOG_INFO, "error is %s", strerror(-req->GetResult()));
+    ASSERT_EQ(-22, req->GetResult());
+    free(buf);
+}
+
+TEST_F(IOManagerUnitTest, MultipleReads)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // populate the file with some data
+    system(FString("/bin/dd bs=1024 count=10000 if=/dev/zero of=" + mTmpfile));
+
+    // Open the file with direct IO
+    AutoFD fd(open(mTmpfile, O_DIRECT));
+    ASSERT_NE(fd.GetFD(), -1);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    // Read a series of 512 byte blocks
+    ssize_t total = 512*512;
+    ssize_t each = 512;
+    char *buf = 0;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/total));
+    memset(buf, 1, total);
+    for(int i = 0; i < (ssize_t)(total/sizeof(int)); ++i)
+    {
+        int *a = (int *)(buf + sizeof(int) * i);
+        if (*a != 0x01010101)
+            hlog(HLOG_ERR, "a != 0:  i=%d buf=%p addr=%p",
+                 i, buf, (int *)(buf + sizeof(int) * i));
+        ASSERT_EQ(0x01010101, *a);
+    }
+    std::map<uint64_t, boost::shared_ptr<IORequest> > requestMap;
+    for (int i = 0; i < total / each; ++i)
+    {
+        // submit IO request
+        boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+        req->SetOp(IORequest::READ);
+        req->SetCallback(IOManagerUnitTest::Notify);
+        req->SetUserData(this);
+        req->SetBuffer(buf+(each*i), each);
+        req->SetOffset(each*i);
+        req->SetFD(fd);
+        uint64_t reqnum;
+        ASSERT_NO_THROW(reqnum = iomgr->SubmitRequest(req));
+        requestMap[reqnum] = req;
+        EXPECT_EQ(requestMap.size(), i+1); // (check for unique reqnums)
+    }
+    Wait(total/each);
+    foreach (const IORequestPair &p, requestMap)
+    {
+        const IORequestPtr &req(p.second);
+        // hlog(HLOG_INFO, "req %lu got result %ld %s", req->GetRequestNumber(),
+        //      req->GetResult(), req->GetResult() > 0 ? "" : strerror(-req->GetResult()));
+        EXPECT_EQ(each, req->GetResult());
+    }
+    for(int i = 0; i < (ssize_t)(total/sizeof(int)); ++i)
+    {
+        int *a = (int *)(buf + sizeof(int) * i);
+        if (*a != 0)
+            hlog(HLOG_ERR, "a != 0:  i=%d buf=%p addr=%p",
+                 i, buf, (int *)(buf + sizeof(int) * i));
+        ASSERT_EQ(0, *a);
+    }
+    ASSERT_EQ(total/each, mNumCompletions);
+    free(buf);
+}
+
+// Multiple Writes
+TEST_F(IOManagerUnitTest, MultipleWrites)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // populate the file with some data
+    system(FString("/bin/dd bs=1024 count=10000 if=/dev/zero of=" + mTmpfile));
+
+    // Open the file with direct IO
+    AutoFD fd(open(mTmpfile, O_RDWR | O_DIRECT));
+    ASSERT_NE(fd.GetFD(), -1);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    // Read a series of 512 byte blocks
+    ssize_t total = 512*512;
+    ssize_t each = 512;
+    char *buf = 0;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/total));
+    memset(buf, 1, total);
+    for(int i = 0; i < (ssize_t)(total/sizeof(int)); ++i)
+    {
+        int *a = (int *)(buf + sizeof(int) * i);
+        if (*a != 0x01010101)
+            hlog(HLOG_ERR, "a != 0:  i=%d buf=%p addr=%p",
+                 i, buf, (int *)(buf + sizeof(int) * i));
+        ASSERT_EQ(0x01010101, *a);
+    }
+    std::map<uint64_t, boost::shared_ptr<IORequest> > requestMap;
+    for (int i = 0; i < total / each; ++i)
+    {
+        // submit IO request
+        boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+        req->SetOp(IORequest::WRITE);
+        req->SetCallback(IOManagerUnitTest::Notify);
+        req->SetUserData(this);
+        req->SetBuffer(buf+(each*i), each);
+        req->SetOffset(each*i);
+        req->SetFD(fd);
+        uint64_t reqnum;
+        ASSERT_NO_THROW(reqnum = iomgr->SubmitRequest(req));
+        requestMap[reqnum] = req;
+        EXPECT_EQ(requestMap.size(), i+1); // (check for unique reqnums)
+    }
+    Wait(total/each);
+    foreach (const IORequestPair &p, requestMap)
+    {
+        const IORequestPtr &req(p.second);
+        // hlog(HLOG_INFO, "req %lu got result %ld %s", req->GetRequestNumber(),
+        //      req->GetResult(), req->GetResult() > 0 ? "" : strerror(-req->GetResult()));
+        EXPECT_EQ(each, req->GetResult());
+    }
+    for(int i = 0; i < (ssize_t)(total/sizeof(int)); ++i)
+    {
+        int *a = (int *)(buf + sizeof(int) * i);
+        if (*a != 0x01010101)
+            hlog(HLOG_ERR, "a != 0x01010101:  i=%d buf=%p addr=%p",
+                 i, buf, (int *)(buf + sizeof(int) * i));
+        ASSERT_EQ(0x01010101, *a);
+    }
+    ASSERT_EQ(total/each, mNumCompletions);
+    free(buf);
+}
+
+// Read/Write Mix
+TEST_F(IOManagerUnitTest, ReadWriteMix)
+{
+    FTRACE;
+    FileSystemImpl fs;
+
+    // populate the file with some data
+    system(FString("/bin/dd bs=1024 count=10000 if=/dev/zero of=" + mTmpfile));
+
+    // Open the file with direct IO
+    AutoFD fd(open(mTmpfile, O_RDWR | O_DIRECT));
+    ASSERT_NE(fd.GetFD(), -1);
+
+    boost::shared_ptr<IOManager> iomgr = boost::make_shared<IOManager>(256);
+
+    ssize_t total = 512*512;
+    ssize_t each = 512;
+    char *buf = 0;
+    ASSERT_EQ(0, posix_memalign((void **)&buf, /*align*/4096, /*size*/total));
+    memset(buf, 1, total);
+    for(int i = 0; i < (ssize_t)(total/sizeof(int)); ++i)
+    {
+        int *a = (int *)(buf + sizeof(int) * i);
+        if (*a != 0x01010101)
+            hlog(HLOG_ERR, "a != 0:  i=%d buf=%p addr=%p",
+                 i, buf, (int *)(buf + sizeof(int) * i));
+        ASSERT_EQ(0x01010101, *a);
+    }
+    std::map<uint64_t, boost::shared_ptr<IORequest> > requestMap;
+    for (int i = 0; i < total / each; ++i)
+    {
+        // submit IO request
+        boost::shared_ptr<IORequest> req = iomgr->NewRequest();
+        if (i % 2 == 0)
+            req->SetOp(IORequest::READ);
+        else
+            req->SetOp(IORequest::WRITE);
+        req->SetCallback(IOManagerUnitTest::Notify);
+        req->SetUserData(this);
+        req->SetBuffer(buf+(each*i), each);
+        req->SetOffset(each*i);
+        req->SetFD(fd);
+        uint64_t reqnum;
+        ASSERT_NO_THROW(reqnum = iomgr->SubmitRequest(req));
+        requestMap[reqnum] = req;
+        EXPECT_EQ(requestMap.size(), i+1); // (check for unique reqnums)
+    }
+    Wait(total/each);
+    foreach (const IORequestPair &p, requestMap)
+    {
+        const IORequestPtr &req(p.second);
+        // hlog(HLOG_INFO, "req %lu got result %ld %s", req->GetRequestNumber(),
+        //      req->GetResult(), req->GetResult() > 0 ? "" : strerror(-req->GetResult()));
+        EXPECT_EQ(each, req->GetResult());
+    }
+    for(int i = 0; i < (ssize_t)(total/sizeof(int)); ++i)
+    {
+        int *a = (int *)(buf + sizeof(int) * i);
+        int b = i / ((total/each) / sizeof(int));
+        if ((b % 2 == 0 && *a != 0) ||
+            (b % 2 != 0 && *a != 0x01010101))
+            hlog(HLOG_ERR, "a has unexpected value of 0x%x:  i=%d b=%d buf=%p addr=%p",
+                 *a, i, b, buf, (int *)(buf + sizeof(int) * i));
+        ASSERT_EQ((b % 2 == 0) ? 0 : 0x01010101, *a);
+    }
+    ASSERT_EQ(total/each, mNumCompletions);
+    free(buf);
+}
+
+// Test aligned read with an odd buffer size, make sure only the
+// desired size of the target buffer is modified.
