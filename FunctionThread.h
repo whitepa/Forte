204d0ebaa9ef1e05f59bb375842befcf65128905
Nate Hardt <Nate Hardt nate@scalecomputing.com>
2013-05-22 14:20:52 -0700

    forte: create a thread based on boost::function
    Change-Id: I468d4e006d0ec79de6481df62b4de101982bd334
    Reviewed-on: http://gerrit.lab.local:8080/4366
    Tested-by: build
    Smoketests: build
    Reviewed-by: Nate Hardt <nate@scalecomputing.com>
    Reviewed-by: Phil White <pwhite@scalecomputing.com>

---
+++ FunctionThread.h
@@ -0,0 +1,45 @@
+#ifndef __Forte_FunctionThread_h__
+#define __Forte_FunctionThread_h__
+
+namespace Forte
+{
+    class FunctionThread : public Forte::Thread
+    {
+    public:
+        FunctionThread(boost::function<void()> f,
+                       const std::string& name)
+            : mFunction(f) {
+            setThreadName(name);
+        }
+
+        class AutoInit {};
+
+        FunctionThread(const AutoInit& throwaway,
+                       boost::function<void()> f,
+                       const std::string& name)
+            : mFunction(f) {
+            setThreadName(name);
+            initialized();
+        }
+
+        ~FunctionThread() {
+            deleting();
+        }
+
+        void Begin() {
+            initialized();
+        }
+
+    protected:
+        virtual void* run() {
+            mFunction();
+            return NULL;
+        }
+
+    protected:
+        boost::function<void()> mFunction;
+    };
+}
+
+#endif /* __Forte_FunctionThread_h__ */
+
