3c773ec419cae9ed765f96dd82712d3111e34fbe
Nate Hardt <nate@scalecomputing.com>
2013-11-21 13:35:49 -0800

    forte: more robust Endpoint FD unit test
    while we know it works, before changing core internals
    
    Change-Id: I2bdef01dec348e43bf2ed02f0dff6c2ea9c7f4c0
    Reviewed-on: http://gerrit.lab.local:8080/6440
    Tested-by: build
    Smoketests: build
    Reviewed-by: Nate Hardt <nate@scalecomputing.com>

--- unittest/PDUPeerEndpointFDUnitTest.cpp
+++ unittest/PDUPeerEndpointFDUnitTest.cpp
@@ -20,7 +20,7 @@ using ::testing::_;
 
 LogManager logManager;
 
-class PDUPeerEndpointFDUnitTest : public ::testing::Test
+class PDUPeerEndpointFDUnitTest : public ::testing::TestWithParam<int>
 {
 public:
     PDUPeerEndpointFDUnitTest()
@@ -108,22 +108,22 @@ public:
         switch (event->mEventType)
         {
         case PDUPeerReceivedPDUEvent:
-            hlog(HLOG_INFO, "Recvd PDU Event");
+            //hlog(HLOG_INFO, "Recvd PDU Event");
             ++mReceiveEventCount;
             break;
 
         case PDUPeerSendErrorEvent:
-            hlog(HLOG_INFO, "Recvd Send Error Event");
+            //hlog(HLOG_INFO, "Recvd Send Error Event");
             ++mSendErrorEventCount;
             break;
 
         case PDUPeerConnectedEvent:
-            hlog(HLOG_INFO, "Recvd Connect Event");
+            //hlog(HLOG_INFO, "Recvd Connect Event");
             ++mConnectedEventCount;
             break;
 
         case PDUPeerDisconnectedEvent:
-            hlog(HLOG_INFO, "Recvd Disconnect Event");
+            //hlog(HLOG_INFO, "Recvd Disconnect Event");
             ++mDisconnectedEventCount;
             break;
 
@@ -163,7 +163,7 @@ struct TestPayload
     char d[128];
 } __attribute__((__packed__));
 
-boost::shared_ptr<PDU> makeTestPDU()
+boost::shared_ptr<PDU> makeTestPDU(size_t optionalDataSize=4096)
 {
     PDUPtr pdu;
 
@@ -172,10 +172,10 @@ boost::shared_ptr<PDU> makeTestPDU()
     header.opcode = 1;
     header.payloadSize = sizeof(TestPayload);
 
-    boost::shared_array<char> optionalDataPayload(new char[4096]);
-    memset(optionalDataPayload.get(), 'z', 4096);
+    boost::shared_array<char> optionalDataPayload(new char[optionalDataSize]);
+    memset(optionalDataPayload.get(), 'z', optionalDataSize);
     boost::shared_ptr<PDUOptionalData> o(
-        new PDUOptionalData(4096, 0, optionalDataPayload.get()));
+        new PDUOptionalData(optionalDataSize, 0, optionalDataPayload.get()));
 
     TestPayload payload;
     payload.a = 1;
@@ -189,11 +189,49 @@ boost::shared_ptr<PDU> makeTestPDU()
     return pdu;
 }
 
+boost::shared_ptr<PDU> makeRandomPDU()
+{
+    PDUPtr pdu;
+
+    size_t optionalDataSize = rand() % (65536 - 4096);
+
+    PDUHeader header;
+    header.version = Forte::PDU::PDU_VERSION;
+    header.opcode = rand() % 127;
+    header.payloadSize = sizeof(TestPayload);
+
+    boost::shared_array<char> optionalDataPayload(new char[optionalDataSize]);
+    for (unsigned int i=0; i<optionalDataSize; ++i)
+    {
+        optionalDataPayload.get()[i] = i;
+    }
+    boost::shared_ptr<PDUOptionalData> o(
+        new PDUOptionalData(optionalDataSize,
+                            10+(rand()%5),
+                            optionalDataPayload.get()));
+
+    TestPayload payload;
+    payload.a = rand();
+    payload.b = payload.a + 1;
+    payload.c = payload.a + 2;
+    memset(payload.d, 0, sizeof(payload.d));
+
+    pdu.reset(new PDU(header.opcode, sizeof(payload), &payload));
+    pdu->SetOptionalData(o);
+
+    return pdu;
+}
+
 size_t testPDUSize(const boost::shared_ptr<PDU>& pdu)
 {
     return sizeof(pdu->GetHeader())
         + pdu->GetPayloadSize()
         + pdu->GetOptionalDataSize();
+}
+
+size_t testPDUMaxSize()
+{
+    return DEFAULT_MAX_BUFFER_SIZE - sizeof(PDUHeader) - sizeof(TestPayload);
 }
 
 TEST_F(PDUPeerEndpointFDUnitTest, ConstructDelete)
@@ -304,6 +342,41 @@ TEST_F(PDUPeerEndpointFDUnitTest, QueuesPDUsForSending)
     teardownDefaultFDPair();
 }
 
+TEST_F(PDUPeerEndpointFDUnitTest, CanSendPDUUpToMaxPayload)
+{
+    FTRACE;
+    setupDefaultFDPair();
+
+    Forte::PDUPtr pdu = makeTestPDU(testPDUMaxSize());
+
+    mPDUQueue1->EnqueuePDU(pdu);
+    Forte::PDU out;
+    while (!mEndpoint2->RecvPDU(out)) { usleep(10000); }
+    ASSERT_EQ(*pdu, out);
+
+    teardownDefaultFDPair();
+}
+
+TEST_F(PDUPeerEndpointFDUnitTest, PDUsCanHaveAnySizePayload)
+{
+    FTRACE;
+    setupDefaultFDPair();
+
+    for (int i = 0; i < 1024; ++i)
+    {
+        size_t thisPDUSize = rand() % testPDUMaxSize();
+        //hlog(HLOG_INFO, "sending PDU with optional payload of %zu", thisPDUSize);
+        Forte::PDUPtr pdu = makeTestPDU(thisPDUSize);
+        mPDUQueue1->EnqueuePDU(pdu);
+
+        Forte::PDU out;
+        while (!mEndpoint2->RecvPDU(out)) { usleep(10000); }
+        ASSERT_EQ(*pdu, out);
+    }
+
+    teardownDefaultFDPair();
+}
+
 TEST_F(PDUPeerEndpointFDUnitTest,
        FiresDisconnect_OnCloseFD_EPollErr)
 {
@@ -342,7 +415,7 @@ TEST_F(PDUPeerSetImplOnBoxTest, CanReceiveDataWhenSendQueueIsFull)
 }
 */
 
-TEST_F(PDUPeerEndpointFDUnitTest, PDUPayoadVersionTest)
+TEST_F(PDUPeerEndpointFDUnitTest, PDUPayloadVersionTest)
 {
     FTRACE;
     setupDefaultFDPair();
@@ -372,6 +445,159 @@ TEST_F(PDUPeerEndpointFDUnitTest, PDUPayoadVersionTest)
         PDUPtr oldPDU = pdus.at(i);
         ASSERT_TRUE(received);
         ASSERT_TRUE(PDU::GetPayloadVersion(out.GetVersion()) == i);
+    }
+
+    teardownDefaultFDPair();
+}
+
+//TODO: mvoe this onbox. it's a baseline perf number for what
+//PDUPeerEndpointFD needs to do
+TEST_F(PDUPeerEndpointFDUnitTest,
+       DISABLED_WillReadWritePDUsAsLongAsThereIsRoom)
+{
+    logManager.EndLogging(__FILE__ ".log");
+
+    setupDefaultFDPair();
+
+    TimerClock c;
+    c.Start();
+    const int iterations(1000);
+    int totalIO(0);
+    Forte::PDU out;
+    Forte::PDUPtr pdu = makeTestPDU();
+
+    for (int i=0; i<iterations; ++i)
+    {
+        while (mEndpoint2->RecvPDU(out))
+        {
+            ASSERT_EQ(*pdu, out);
+            ++totalIO;
+        }
+
+        try
+        {
+            while (true)
+            {
+                mPDUQueue1->EnqueuePDU(pdu);
+                ++totalIO;
+            }
+        }
+        catch (EPDUQueueFull& e)
+        {
+        }
+    }
+
+    c.Stop();
+    hlogstream(HLOG_INFO,
+               iterations << " iterations took " << c.GetTime()
+               << " and processed " << totalIO);
+
+    //memmove impl baseline on laptop with ASSERT_EQ commented out
+    //1000 iterations took 58.369622753 and processed 1003371
+
+    teardownDefaultFDPair();
+
+    logManager.BeginLogging(__FILE__ ".log", HLOG_ALL);
+}
+
+TEST_P(PDUPeerEndpointFDUnitTest, PDUsCanHaveConsistentSizePayloads)
+{
+    FTRACE;
+    setupDefaultFDPair();
+
+    Forte::PDUPtr pdu = makeTestPDU(GetParam());
+    Forte::PDU out;
+
+    //TODO: make an onbox test
+    //for (int i = 0; i < 6400*3; ++i)
+    for (int i = 0; i < 256; ++i)
+    {
+        try
+        {
+            mPDUQueue1->EnqueuePDU(pdu);
+        }
+        catch(...)
+        {
+            // in case of queue full
+        }
+        if (i % 3 == 0)
+        {
+            while (mEndpoint2->RecvPDU(out)) {
+                ASSERT_EQ(*pdu, out);
+            }
+        }
+    }
+
+    teardownDefaultFDPair();
+}
+
+INSTANTIATE_TEST_CASE_P(PDUPayloads,
+                        PDUPeerEndpointFDUnitTest,
+                        ::testing::Values(1, 16384, 114688));
+
+
+//TODO: this would be a useful tests
+// TEST_F(PDUPeerSetImplOnBoxTest, ExpandsAndCopiesRingBufferWhenFull)
+// {
+//     mMonitor.reset(new EPollMonitor);
+//     mPDUQueue1.reset(new PDUQueue);
+//     mPDUQueue2.reset(new PDUQueue);
+
+//     mMonitor->Start();
+
+//     int fds[2];
+//     socketpair(AF_UNIX, SOCK_STREAM, 0, fds);
+//     mFD1 = fds[0];
+//     mFD2 = fds[1];
+
+//     mEndpoint1.reset(
+//         new PDUPeerEndpointFD(mPDUQueue1,
+//                               mMonitor,
+//                               TODO: change queue sizes
+//                               ));
+//     mEndpoint2.reset(
+//         new PDUPeerEndpointFD(mPDUQueue2,
+//                               mMonitor,
+//                               TODO: change queue sizes
+//                               ));
+
+//     mEndpoint1->SetEventCallback(
+//         boost::bind(
+//             &PDUPeerEndpointFDUnitTest::EventCallback, this, _1));
+
+//     mEndpoint2->SetEventCallback(
+//         boost::bind(
+//             &PDUPeerEndpointFDUnitTest::EventCallback, this, _1));
+
+//     mEndpoint1->SetFD(mFD1);
+//     mEndpoint2->SetFD(mFD2);
+
+//     mEndpoint1->Start();
+//     mEndpoint2->Start();
+
+//     //TODO: test goes here
+//     mPDUQueue1->EnqueuePDU(pdu);
+//     while (!mEndpoint2->RecvPDU(out));
+//     ASSERT_EQ(*pdu, out);
+//
+//teardownDefaultFDPair()
+// }
+
+TEST_F(PDUPeerEndpointFDUnitTest, CanSendAndVerifyAnythingMathingPDUSpec)
+{
+    FTRACE;
+
+    setupDefaultFDPair();
+
+    Forte::PDUPtr pdu = makeRandomPDU();
+    Forte::PDU out;
+
+    //TODO: onbox test for (int i=0; i<65536; ++i)
+    for (int i=0; i<256; ++i)
+    {
+        mPDUQueue1->EnqueuePDU(pdu);
+        ASSERT_NO_THROW(while (!mEndpoint2->RecvPDU(out)));
+        ASSERT_EQ(*pdu, out);
     }
 
     teardownDefaultFDPair();
