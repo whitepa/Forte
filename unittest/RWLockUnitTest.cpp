b51803d882768706040e4734e3a88185a8a3425a
Nate Hardt <nate@scalecomputing.com>
2013-12-04 22:11:58 -0800

    forte: concurrency test for RWLock
    Change-Id: I120334883a6ad90cd0754cf6f72089a5d3247054
    Reviewed-on: http://gerrit.lab.local:8080/6717
    Tested-by: build
    Smoketests: build
    Reviewed-by: Nate Hardt <nate@scalecomputing.com>

--- unittest/RWLockUnitTest.cpp
+++ unittest/RWLockUnitTest.cpp
@@ -1,7 +1,10 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "FTrace.h"
 #include "LogManager.h"
+#include "FunctionThread.h"
+
+#include <boost/bind.hpp>
 
 #include "RWLock.h"
 
@@ -56,4 +59,95 @@ TEST_F(RWLockUnitTest, MultipleReadLocks)
         AutoReadUnlock lock2(rwl);
         AutoReadUnlock lock3(rwl);
     }
+}
+
+struct ReaderObj
+{
+public:
+    ReaderObj() : mAssertIfRead(false) {}
+
+    void Read() {
+        if (mAssertIfRead) {
+            abort();
+        }
+    }
+
+    void SetAssertIfRead(bool assertIfRead) {
+        if (mAssertIfRead == assertIfRead)
+        {
+            abort();
+        }
+        mAssertIfRead = assertIfRead;
+    }
+
+private:
+    bool mAssertIfRead;
+};
+
+void GetReadLockThread(RWLock& rwl, ReaderObj& obj)
+{
+    for (int i=0; i<10000; ++i)
+    {
+        AutoReadUnlock lock(rwl);
+        obj.Read();
+    }
+    hlog(HLOG_INFO, "Read thread complete");
+}
+
+void GetWriteLockThread(RWLock& rwl, ReaderObj& obj)
+{
+    for (int i=0; i<100; ++i)
+    {
+        usleep(1000);
+        AutoWriteUnlock lock(rwl);
+        obj.SetAssertIfRead(true);
+        usleep(10000);
+        obj.SetAssertIfRead(false);
+    }
+    hlog(HLOG_INFO, "Write thread complete");
+}
+
+TEST_F(RWLockUnitTest, CanQueueWriteLock_TriggerReaders_ProtectedData)
+{
+    FTRACE;
+    RWLock rwl;
+    ReaderObj obj;
+
+    std::vector<boost::shared_ptr<FunctionThread> > threadGroup;
+
+    // readers
+    for (int i=0; i<10; ++i)
+    {
+        threadGroup.push_back(
+            boost::shared_ptr<FunctionThread>(
+                new FunctionThread(
+                    boost::bind(&GetReadLockThread,
+                                boost::ref(rwl),
+                                boost::ref(obj)),
+                    "rdr")));
+    }
+
+
+    // writers
+    for (int i=0; i<10; ++i)
+    {
+        threadGroup.push_back(
+            boost::shared_ptr<FunctionThread>(
+                new FunctionThread(
+                    boost::bind(&GetWriteLockThread,
+                                boost::ref(rwl),
+                                boost::ref(obj)),
+                    "writer"
+                    )));
+    }
+
+    std::for_each(
+        threadGroup.begin(),
+        threadGroup.end(),
+        boost::bind(&FunctionThread::Begin, _1));
+
+    std::for_each(
+        threadGroup.begin(),
+        threadGroup.end(),
+        boost::bind(&FunctionThread::WaitForShutdown, _1));
 }
